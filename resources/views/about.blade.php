822ccc07a639f06c66276d1e195edb0ae9ea8566
4binn <bintangstu56@gmail.com>
2025-11-17 12:29:26 +0700

    memperbaiki tampilan about dan contact

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+      <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <!-- Your content -->
+      <div class="text-lg text-white">About Pages</div>
+    </div>
+  </main>
+</div>
+</x-layout>
