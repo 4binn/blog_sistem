50c287d1a399a6536b1ddb71f10c9bed2c02f0f9
4binn <bintangstu56@gmail.com>
2025-11-17 21:54:50 +0700

    membuat navigasi link termasuk mobile menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,15 +10,15 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-              <a href="/" aria-current="page" class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-              <a href="blog" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-              <a href="about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About Us</a>
-              <a href="contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+              <x-navbar-link href="/" :current="request()->is('/')">Home</x-navbar-link>
+              <x-navbar-link href="/blog" :current="request()->is('blog')">blog</x-navbar-link>
+              <x-navbar-link href="/about" :current="request()->is('about')"> about </x-navbar-link>
+              <x-navbar-link href="/contact" :current="request()->is('contact')"> contact </x-navbar-link>
             </div>
           </div>
         </div>
         <div class="hidden md:block" x-data="{ isOpen:false }">
-  <div class="ml-4 flex items-center md:ml-6">
+         <div class="ml-4 flex items-center md:ml-6">
 
     <!-- Profile dropdown -->
     <div class="relative ml-3">
@@ -69,11 +69,10 @@
      x-transition:leave-end="opacity-0 transform -translate-y-2"
     class="md:hidden bg-gray-800 border-t border-white/10 pb-3">
     <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-        <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-        <a href="/" aria-current="page" class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
-        <a href="blog" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-        <a href="about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-        <a href="contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+        <x-navbar-link class="block" href="/" :current="request()->is('/')">Home</x-navbar-link>
+        <x-navbar-link class="block" href="/blog" :current="request()->is('blog')">blog</x-navbar-link>
+        <x-navbar-link class="block" href="/about" :current="request()->is('about')"> about </x-navbar-link>
+        <x-navbar-link class="block" href="/contact" :current="request()->is('contact')"> contact </x-navbar-link>
       </div>
       <div class="border-t border-white/10 pt-4 pb-3">
         <div class="flex items-center px-5">
